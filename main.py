df7f5074a4bbf14e8e11c3897472c9a29f6bb352
Logapradheep12 <leoprasath7@gmail.com>
2025-04-10 21:00:05 +0530

    More Learning

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 
 
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -8,19 +10,70 @@ app = FastAPI()
 def index():
     return "Heyyyy"
 
+
+
 @app.get("/index_1")
 def index_1():
     return "What's going on there"
 
-@app.get("/about")
-def json():
-    return {"Data":"About Page"}
 
 
 @app.get("/index_1/json")
 def json():
     return {"data":{"name": "Pradheep"}}
 
+
+
 @app.get("/index_1/json/json_1")
 def json():
     return {"data":{"Json data's are there"}}
+
+
+
+@app.get("/blog/unpublished")
+def blog():
+    return {"Data" : "All Unpublished Blogs"}
+
+
+
+# fetch /blog with id = id
+
+@app.get("/blog/{id}")
+def json(id: int):
+    return {"Data" : id}
+
+
+
+# fetch comments /blog with id = id
+
+@app.get("/blog/{id}/comment")
+def json(id):
+    return {"Data" : {"1","2"}}
+
+
+
+
+# only get 10 API In the blog
+
+@app.get("/blog")
+def json(limit):
+    return {"Data" : f'{limit} of Blog Values'}
+
+
+
+# request blog
+
+class Blog(BaseModel):
+    title : str
+    body : str
+    published : Optional[bool]
+    
+    
+@app.post('/blog1')
+def post_blog(blog : Blog):
+    return {'data' : f'Blog is created and its title {blog.title}. then its {blog.body}'}
+    
+    
+# Debugging
+
+
